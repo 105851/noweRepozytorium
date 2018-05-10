d748f19742d0f14be55700f6826a8d8738aef438
105851 <48195@student.ue.poznan.pl>
2018-05-10 12:07:25 +0200

    kalkulator

---
+++ kalk.py
@@ -0,0 +1,13 @@
+def odejmowanie():
+    a = raw_input("Pierwsza liczba ")
+    b = raw_input("Druga liczba ")
+    wynik = float(a)-float(b)
+    return wynik
+
+def dodawanie():
+    a = raw_input("Pierwsza liczba ")
+    b = raw_input("Druga liczba ")
+    wynik = float(a)+float(b)
+    return wynik
+
+print(wynik())
